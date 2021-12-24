e2955b28071b630a2db387d9f1c7bd0472d9d610
flexiszp <flexiszp@gmail.com>
2021-12-24 16:44:02 +0200

    sixth commit

--- src/Components/Profile/Profile.jsx
+++ src/Components/Profile/Profile.jsx
@@ -8,7 +8,18 @@ const Profile = () => {
         Minima nostrum quam commodi debitis esse sapiente similique quaerat harum corporis quia quod, repellendus, corrupti architecto illo ut dicta? Deleniti repellat atque asperiores sequi voluptatibus aperiam vero quam fugit sunt?
         Est architecto minima quis molestiae pariatur obcaecati molestias, quasi similique odio porro voluptatibus laborum, eaque eveniet tenetur! Cum necessitatibus non maiores odio illum delectus consequatur ab sed nobis hic. Itaque!
         Ut, iusto reiciendis animi saepe reprehenderit incidunt quibusdam ullam perferendis in sit dolore similique nemo dignissimos quo enim voluptatibus. Sit fuga id reprehenderit veritatis vero nesciunt error saepe deserunt. Amet. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Inventore, accusantium sequi minus soluta blanditiis sed, libero quisquam nemo consequatur unde explicabo. Explicabo illum alias quidem cupiditate saepe laboriosam omnis quasi?
-        Velit officia fugit vero tempore nam perspiciatis quos, id similique incidunt fugiat obcaecati tenetur repellendus sed blanditiis, dolorum voluptate voluptates. Autem placeat qui ea natus doloribus dolorem minus quam delectus? </div>
+        Velit officia fugit vero tempore nam perspiciatis quos, id similique incidunt fugiat obcaecati tenetur repellendus sed blanditiis, dolorum voluptate voluptates. Autem placeat qui ea natus doloribus dolorem minus quam delectus? Lorem ipsum dolor sit, amet consectetur adipisicing elit. Suscipit, numquam architecto, praesentium beatae facilis aperiam eveniet vitae minima officia veritatis magni. Eligendi neque voluptas consequatur vero placeat dolor quidem atque.
+        Id recusandae iste quam repellat in, incidunt veniam labore accusantium repudiandae nihil consequuntur porro quos debitis corporis maxime earum nostrum fuga ut amet at? Beatae molestiae atque nobis harum temporibus? 
+        
+        
+        <br></br>
+        <br></br>
+        <div className={s.item}>TITLE</div>
+        <div className={s.item}>TITLE_ADD</div>
+        
+        </div>
+
+        
 
     )
 
