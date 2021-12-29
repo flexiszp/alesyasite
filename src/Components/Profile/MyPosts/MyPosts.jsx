e163b6e1cca6f01e14077f386677e658bdc27f0c
flexiszp <flexiszp@gmail.com>
2021-12-29 16:07:31 +0200

    commit_29122021

--- src/Components/Profile/MyPosts/MyPosts.jsx
+++ src/Components/Profile/MyPosts/MyPosts.jsx
@@ -2,26 +2,27 @@ import React from 'react';
 import Post from './Post/Post';
 
 const MyPosts = () => {
+
+
     return (
 
-    <div>
-        <textarea></textarea>
-        <button>Add Post</button>
-        <button>Remove</button>
-        
         <div>
-        <Post />
-        <Post />
-        <Post />
-        <Post />
+            <textarea></textarea>
+            <button>Add Post</button>
+            <button>Remove</button>
 
-            
+            <div>
+
+                <Post message='HI, how are you?' />
+                <Post message='HI men yeahh?' />
+                
+
+            </div>
         </div>
-    </div>
-        
+
     )
 
-    
+
 }
 
 export default MyPosts;
--- src/Components/Profile/MyPosts/Post/Post.jsx
+++ src/Components/Profile/MyPosts/Post/Post.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import s from './Post.module.css';
 
-const Post = () => {
+const Post = (props) => {
+    
     return (
 
         <div className={s.item}>
             <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTXVVn-g4kdC97M5d7yILBw4shv_9FTvbUAcA&usqp=CAU' />
-            -POST-
+
+            {props.message}
 
             <div>
                 <span>like</span>
