364e970d580a90ef63bc6f70a6023f60bb2f5773
flexiszp <flexiszp@gmail.com>
2022-01-04 16:08:36 +0200

    commit_04012022

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import Header from './Components/Header/Header.jsx';
 import Profile from './Components/Profile/Profile.jsx';
 import Navbar from './Components/Navbar/Navbar.jsx';
+import Dialogs from './Components/Dialogs/Dialogs.jsx';
 
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
      <Header />
      <Navbar />
      <Profile />
+     <Dialogs />
      <div className='New'>Персональная консультация </div>
      <div className='New1'>Подбор персонала </div>
      <div className='New2'>Социальная поддержка</div>
