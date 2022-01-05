4e62febf53fb84e55f9f2930587f3635e6cf8d90
flexiszp <flexiszp@gmail.com>
2022-01-05 17:17:03 +0200

    commit_05012022_2

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,16 +1,17 @@
 import React from 'react';
 import s from './Navbar.module.css';
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
     return (
         <nav className={s.Nav}>
 
-            <div className={s.item}><a href='/Main'>Главная</a></div>
-            <div className={s.item}><a href='/Articles'>Статьи</a></div>
-            <div className={s.item}><a href='/Prices'>Стоимость</a></div>
-            <div className={s.item}><a href='/Contacts'>Контакты</a></div>
-            <div className={s.item}><a href='/Knowlege'>База знаний</a></div>
-            <div className={s.item}><a href='/Comments'>Популярные вопросы</a></div>
+            <div className={s.item}><Link to='/Main'>Главная</Link></div>
+            <div className={s.item}><Link to='/Articles'>Статьи</Link></div>
+            <div className={s.item}><Link to='/Prices'>Стоимость</Link></div>
+            <div className={s.item}><Link to='/Contacts'>Контакты</Link></div>
+            <div className={s.item}><Link to='/Knowlege'>База знаний</Link></div>
+            <div className={s.item}><Link to='/Comments'>Популярные вопросы</Link></div>
 
 
         </nav>
