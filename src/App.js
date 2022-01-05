e968ab0901bcb846ff690e08fdd9aa59a4c84352
flexiszp <flexiszp@gmail.com>
2022-01-05 11:28:40 +0200

    commit_05012022

--- src/App.js
+++ src/App.js
@@ -3,24 +3,43 @@ import Header from './Components/Header/Header.jsx';
 import Profile from './Components/Profile/Profile.jsx';
 import Navbar from './Components/Navbar/Navbar.jsx';
 import Dialogs from './Components/Dialogs/Dialogs.jsx';
+import {BrowserRouter, Routes, Route} from "react-router-dom";
 
 
 function App() {
   return (
-    <div className="wrapper">
+  
+	<BrowserRouter>
+	
+		<div className="wrapper">
 
-     <Header />
-     <Navbar />
-     {/*<div class='app-wrapper-content'><Dialogs /></div>*/}
-     <div class='app-wrapper-content'> <Profile /></div>
+		<Header />
+		<Navbar />
+		{/* <div class='app-wrapper-content'><Dialogs /></div>
+		<div class='app-wrapper-content'> <Profile /></div> */}
+		
+		<div className='app-wrapper-content'>
+		
+		<Routes>
+			
+			<Route path='/Profile' element={<Profile />} />
+			<Route path='/Dialogs' element={<Dialogs />} />
+			{/* <Route path='*' element={<Notfoundpage />} /> */}
+			
+		</Routes>
+		
+		</div>
+		
+		
 
-     <div className='New'>Персональная консультация </div>
-     <div className='New1'>Подбор персонала </div>
-     <div className='New2'>Социальная поддержка</div>
-     <div className='footer'>Контент футер</div>
+		<div className='New'>Персональная консультация </div>
+		<div className='New1'>Подбор персонала </div>
+		<div className='New2'>Социальная поддержка</div>
+		<div className='footer'>Контент футер</div>
 
-     </div>
-  )
+		</div>
+		
+	</BrowserRouter>)
 }
 
 
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -5,11 +5,11 @@ const Navbar = () => {
     return (
         <nav className={s.Nav}>
 
-            <div className={s.item}><a href='/main'>Главная</a></div>
-            <div className={s.item}><a href='/feedback'>Обратная связь</a></div>
-            <div className={s.item}><a href='/prices'>Стоимость</a></div>
-            <div className={s.item}><a href='/contacts'>Контакты</a></div>
-            <div className={s.item}><a href='/comments'>Отзывы</a></div>
+            <div className={s.item}><a href='/Profile'>Главная</a></div>
+            <div className={s.item}><a href='/Dialogs'>Обратная связь</a></div>
+            <div className={s.item}><a href='/Prices'>Стоимость</a></div>
+            <div className={s.item}><a href='/Contacts'>Контакты</a></div>
+            <div className={s.item}><a href='/Comments'>Отзывы</a></div>
 
 
         </nav>
